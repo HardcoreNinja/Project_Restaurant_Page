80e5eb318277eee96f65d02109e902f50a57a821
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-08-17 11:29:54 -0400

    Created contact page.
    Created contact page.

--- src/contactContent.js
+++ src/contactContent.js
@@ -10,7 +10,48 @@ function createContactContent() {
     let logoContainer = document.createElement("div");
     logoContainer.classList.add("logoContainer");
 
-    bodyContainer.append(logoContainer);
+    let form = document.createElement("form");
+
+    let nameDiv = document.createElement("div");
+    nameDiv.classList.add("inputContainer");
+    let nameLabel = document.createElement("label");
+    nameLabel.for = "name";
+    nameLabel.innerHTML = "Name:";
+    let nameInput = document.createElement("input");
+    nameInput.setAttribute("id", "name");
+    nameInput.type = "text";
+    nameInput.required = true;
+    nameDiv.append(nameLabel, nameInput);
+
+    let subjectDiv = document.createElement("div");
+    subjectDiv.classList.add("inputContainer");
+    let subjectLabel = document.createElement("label");
+    subjectLabel.for = "subject";
+    subjectLabel.innerHTML = "Subject:";
+    let subjectInput = document.createElement("input");
+    subjectInput.setAttribute("id", "subject");
+    subjectInput.type = "text";
+    subjectInput.required = true;
+    subjectDiv.append(subjectLabel, subjectInput);
+
+    let messageDiv = document.createElement("div");
+    messageDiv.classList.add("inputContainer");
+    let messageLabel = document.createElement("label");
+    messageLabel.for = "message";
+    messageLabel.innerHTML = "Message:";
+    let messageInput = document.createElement("textarea");
+    messageInput.setAttribute("id", "message");
+    messageInput.classList.add("message");
+    messageInput.required = true;
+    messageDiv.append(messageLabel, messageInput);
+
+    let submitButton = document.createElement("button");
+    submitButton.type = "submit";
+    submitButton.innerHTML = "Submit >"
+    submitButton.classList.add("submitButton");
+
+    form.append(nameDiv, subjectDiv, messageDiv, submitButton);
+    bodyContainer.append(logoContainer, form);
     body.append(bodyContainer);
 }
 
